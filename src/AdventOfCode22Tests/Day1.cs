fbdc12c325ca5abf44ea1e1f7b19a87c898a3fd2
Denis Ekart <denis.ekart@gmail.com>
2022-12-01 07:43:08 +0100

    feat: day 1

---
+++ src/AdventOfCode22Tests/Day1.cs
@@ -0,0 +1,66 @@
+﻿namespace AdventOfCode22Tests;
+
+public class Day1
+{
+    [TestCase("""
+        1000
+        2000
+        3000
+
+        4000
+
+        5000
+        6000
+
+        7000
+        8000
+        9000
+
+        10000
+        """, ExpectedResult = 24000)]
+    public int Part1_ShouldReturnGroupWithMaxCaloriesWhenGivenAValidInputString(string input)
+    {
+        var maxCalories = input
+            .Split($"{Environment.NewLine}{Environment.NewLine}")
+            .Select(group => group.Split(Environment.NewLine))
+            .Select(group => group.Select(item => item.ConvertToInteger()))
+            .Select(group => group.Sum())
+            .Max();
+
+        Console.WriteLine($"Puzzle solution: {maxCalories}");
+
+        return maxCalories;
+    }
+
+    [TestCase("""
+        1000
+        2000
+        3000
+
+        4000
+
+        5000
+        6000
+
+        7000
+        8000
+        9000
+
+        10000
+        """, ExpectedResult = 45000)]
+    public int Part2_ShouldReturnSumOfTop3GroupsWithMostCaloriesWhenGivenAValidInputString(string input)
+    {
+        var maxCalories = input
+            .Split($"{Environment.NewLine}{Environment.NewLine}")
+            .Select(group => group.Split(Environment.NewLine))
+            .Select(group => group.Select(item => item.ConvertToInteger()))
+            .Select(group => group.Sum())
+            .OrderByDescending(x => x)
+            .Take(3)
+            .Sum();
+
+        Console.WriteLine($"Puzzle solution: {maxCalories}");
+
+        return maxCalories;
+    }
+}
---
+++ src/AdventOfCode22Tests/LinqExtensions.cs
@@ -0,0 +1,6 @@
+﻿namespace AdventOfCode22Tests;
+
+public static class LinqExtensions
+{
+    public static int ConvertToInteger(this string input, int defaultValue = default) => int.TryParse(input, out var integer) ? integer : defaultValue;
+}
